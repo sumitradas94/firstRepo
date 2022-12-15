be562e128d6a90b22331c5008cb20bdbc232122a
sumitradas94 <120413479+sumitradas94@users.noreply.github.com>
2022-12-16 01:06:09 +0530

    Delete Day3_Assignment directory

--- Day3_Assignment/10thProblem/loop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-echo 'dldir' | program
-if [ -d "$dldir" ]
-then 
-     mkdir $dldir;
-else
-     echo "folder already exist";
-fi
--- Day3_Assignment/4thProblem/forloop.sh
+++
@@ -1,6 +0,0 @@
-for file find /var/log -mtime -7 -ls
-do
-    fileName=ècho $file`;
-    echo $fileName;
-    mv $file ~/backup;
-done
